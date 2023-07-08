328147f664c9ef95f1bd7e9d3463c34e60a8f4f5
royalssecaleb <134155101+royalssecaleb@users.noreply.github.com>
2023-07-08 05:02:55 -0700

    1 file modified

--- index.js
+++ index.js
@@ -1,8 +1,58 @@
-const test = (n) => {
-  if (n == 0 || n == 1) {
-    return String(n);
+function MinWindowSubstring(strArr) {
+  const N = strArr[0];
+  const K = strArr[1];
+  
+  // Create a frequency map for characters in K
+  const charFreqK = {};
+  for (let i = 0; i < K.length; i++) {
+    const char = K[i];
+    charFreqK[char] = charFreqK[char] ? charFreqK[char] + 1 : 1;
   }
-  return test(Math.floor(n / 2)) + String(n % 2);
-};
-
-console.log(test(101));
+  
+  let windowStart = 0;
+  let windowEnd = 0;
+  let minLength = Infinity;
+  let minWindow = "";
+  let charCount = K.length;
+  
+  // Slide the window across N
+  while (windowEnd < N.length) {
+    const charEnd = N[windowEnd];
+    
+    // If the character is in K, decrement its count
+    if (charFreqK[charEnd] !== undefined) {
+      if (charFreqK[charEnd] > 0) {
+        charCount--;
+      }
+      charFreqK[charEnd]--;
+    }
+    
+    // If all characters in K are found, try to minimize the window
+    while (charCount === 0) {
+      const currentWindowLength = windowEnd - windowStart + 1;
+      
+      if (currentWindowLength < minLength) {
+        minLength = currentWindowLength;
+        minWindow = N.substring(windowStart, windowEnd + 1);
+      }
+      
+      const charStart = N[windowStart];
+      
+      // If the character at windowStart is in K, increment its count
+      if (charFreqK[charStart] !== undefined) {
+        charFreqK[charStart]++;
+        
+        // If the count becomes positive, it means we need to find it again
+        if (charFreqK[charStart] > 0) {
+          charCount++;
+        }
+      }
+      
+      windowStart++;
+    }
+    
+    windowEnd++;
+  }
+  
+  return minWindow;
+}
